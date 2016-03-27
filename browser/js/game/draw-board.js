99fb25aed394b662fc8b94fe6389880457c0380a
Emma Bishop <emma.b.bishop@gmail.com>
2016-03-26 22:17:35 -0400

    Renders board using ng-repeat

--- browser/js/game/draw-board.js
+++ browser/js/game/draw-board.js
@@ -1,24 +1,26 @@
-window.start = function(){
-  document.getElementById("board").appendChild(renderer.view);
-}
+// window.start = function(){
+//   console.log('window.start')
+//   document.getElementById("board").appendChild(renderer.view);
+// }
 
-var stage = new PIXI.Stage(0x66ff99);
-var renderer = new PIXI.CanvasRenderer(640,480);
 
-//all image files are 150px. Do not change this number!
-var imgSizeActual = 150 
+// var stage = new PIXI.Stage(0x66ff99);
+// var renderer = new PIXI.CanvasRenderer(640,480);
 
-//factor to rescale images by. This number can be changed
-var imgScale = 3.75 
+// //all image files are 150px. Do not change this number!
+// var imgSizeActual = 150
 
-var imgSize = imgSizeActual/imgScale
-var cols = 12;
-var rows = 16;
+// //factor to rescale images by. This number can be changed
+// var imgScale = 3.75
+
+// var imgSize = imgSizeActual/imgScale
+// var cols = 12;
+// var rows = 16;
 // var board = {
 //           col0: [1, 13, 5, 11, 5, 1, 1, 5, 13, 5, 1, 30, 1, 1, 1, 1],
 //           col1: [4, 100, 1, 11, 1, 1, 1, 90, 13, 1, 52, 2, 1, 1, 5, 1],
-//           col2: [2, 13, 36, 11, 1, 60, 1, 1, 13, 60, 1, 1, 2, 1, 1, 4], 
-//           col3: [1, 13, 4, 1, 40, 41, 96, 1, 41, 10, 10, 10, 1, 1, 5, 1], 
+//           col2: [2, 13, 36, 11, 1, 60, 1, 1, 13, 60, 1, 1, 2, 1, 1, 4],
+//           col3: [1, 13, 4, 1, 40, 41, 96, 1, 41, 10, 10, 10, 1, 1, 5, 1],
 //           col4: [2, 13, 93, 71, 1, 1, 13, 1, 1, 50, 1, 4, 2, 1, 1, 4],
 //           col5: [12, 15, 41, 71, 1, 1, 13, 60, 93, 1, 5, 1, 1, 1, 5, 1],
 //           col6: [10, 10, 40, 71, 1, 36, 22, 10, 40, 10, 10, 10, 1, 1, 5, 1],
@@ -30,51 +32,55 @@ var rows = 16;
 //         }
 
 
-             
-var boardArr = [];
 
-function createBoardArr (boardObj) {
-  for(var key in board) {
-    if(key.slice(0,3) === 'col') boardArr.push(board[key])
-  }
-  return boardArr;
-}
+// var boardArr = [];
 
-function drawDockLine() {
-  var line = new PIXI.Graphics;
-  line.lineStyle(4, 0x000000, 1);
-  line.moveTo(12*imgSizeActual/imgScale, 0)
-  line.lineTo(12*imgSizeActual/imgScale, 12*imgSizeActual/imgScale)
+// function createBoardArr (boardObj) {
+//   console.log('createBoardArr');
+//   for(var key in board) {
+//     if(key.slice(0,3) === 'col') boardArr.push(board[key])
+//   }
+//   return boardArr;
+// }
 
-  stage.addChild(line)
-}
+// function drawDockLine() {
+//   console.log('drawDockLine');
+//   var line = new PIXI.Graphics;
+//   line.lineStyle(4, 0x000000, 1);
+//   line.moveTo(12*imgSizeActual/imgScale, 0)
+//   line.lineTo(12*imgSizeActual/imgScale, 12*imgSizeActual/imgScale)
 
-function buildTiles() {
-  for (var col = 0; col < cols; col ++){
-    for (var row = 0; row < rows; row ++){
-      var tileSrc = '/img/tiles/' + boardArr[col][row] + '.jpg';
-                                                          //150x150 is the actual image size
-      var tile = new PIXI.extras.TilingSprite.fromImage(tileSrc, imgSizeActual, imgSizeActual)
-      
-      tile.position.x = imgSize*row
-      tile.position.y = imgSize*cols - imgSize - imgSize * col;
-      //rescales the 150px tile image to be 4 times smaller 
-      tile.scale.set(1/imgScale, 1/imgScale);
+//   stage.addChild(line)
+// }
 
-      stage.addChild(tile)
-    }
-  }
-}
+// function buildTiles() {
+//   console.log('buildTiles');
+//   for (var col = 0; col < cols; col ++){
+//     for (var row = 0; row < rows; row ++){
+//       var tileSrc = '/img/tiles/' + boardArr[col][row] + '.jpg';
+//                                                           //150x150 is the actual image size
+//       var tile = new PIXI.extras.TilingSprite.fromImage(tileSrc, imgSizeActual, imgSizeActual)
+//       tile.position.x = imgSize*row
+//       tile.position.y = imgSize*cols - imgSize - imgSize * col;
+//       //rescales the 150px tile image to be 4 times smaller
+//       tile.scale.set(1/imgScale, 1/imgScale);
 
-function buildMap(){
-  createBoardArr();
-  buildTiles();
-  drawDockLine();
-}
+//       stage.addChild(tile)
+//     }
+//   }
+// }
+
+// function buildMap(){
+//   console.log('buildMap', board);
+//   createBoardArr();
+//   buildTiles();
+//   drawDockLine();
+// }
 
 
-function init(){
-  renderer.render(stage);
-}
+// function init(){
+//   console.log('init', renderer.view)
+//   renderer.render(stage);
+// }
 
-buildMap();
+// buildMap();
--- browser/js/game/game.controller.js
+++ browser/js/game/game.controller.js
@@ -1,6 +1,21 @@
 app.controller('GameCtrl', function($scope, $state, theGame){
 
 	// window.start()
-  $scope.game = theGame;  
+  $scope.game = theGame;
+  $scope.boardObj = $scope.game.board
+
+  function collectOneCol(n){
+    var key = 'col' + n.toString();
+    var idents = $scope.boardObj[key].map(function(tile){
+      return tile.identifier;
+    });
+    return idents;
+  }
+
+  $scope.board = [];
+  for(var i = 11; i >= 0; i --){
+    $scope.board.push(collectOneCol(i));
+  }
+
 
 });
