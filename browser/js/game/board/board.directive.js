5c4d63a617ba2a1e8ba1f30136d87b60818e97f8
Emma Bishop <emma.b.bishop@gmail.com>
2016-03-26 22:19:16 -0400

    cleans up board directive

--- browser/js/game/board/board.directive.js
+++ browser/js/game/board/board.directive.js
@@ -2,19 +2,6 @@ app.directive('board', function ($parse) {
   return {
     template: '<table><tr ng-repeat="col in board"><td ng-repeat="tile in col track by $index"><img src="/img/tiles/{{tile}}.jpg"></td></tr></table>',
     restrict: 'E',
-    scope: {board: '='},
-    controller: function postLink($scope, $element, $attrs) {
-      var stage = new PIXI.Stage(0x66ff99);
-      var renderer = new PIXI.CanvasRenderer(640,480);
-      $scope.renderPlz = function(){
-        console.log('renderPlz')
-        renderer.render(stage);
-      }
-
-    },
-    link: function(scope){
-      console.log('link')
-      scope.renderPlz()
-    }
+    scope: {board: '='}
   };
 });
