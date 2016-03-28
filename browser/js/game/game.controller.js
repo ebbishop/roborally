2e3784057efd32b6e761dfa768c5d5bdc27c29e4
Jisoo Shin <jisooxshin@gmail.com>
2016-03-28 13:30:18 -0400

    robots are moving in order

--- browser/js/game/game.controller.js
+++ browser/js/game/game.controller.js
@@ -1,4 +1,4 @@
-app.controller('GameCtrl', function($scope, $state, theGame){
+app.controller('GameCtrl', function($scope, $state, theGame, $q){
 
 	$scope.game = theGame;
 	$scope.boardObj = $scope.game.board
@@ -122,24 +122,32 @@ app.controller('GameCtrl', function($scope, $state, theGame){
 		var player1, player2, player3;
 		//seed for original location
 		var players = [
-		  { name: "player1", location: [15,5], bearing: [-1, 0], robot: "Hammer Bot", priorityVal: null },
-		  { name: "player2", location: [15,6], bearing: [-1, 0], robot: "Spin Bot", priorityVal: null },
 		  { name: "player3", location: [14,3], bearing: [-1, 0], robot: "Twonky", priorityVal: null },
+		  { name: "player1", location: [14,5], bearing: [-1, 0], robot: "Hammer Bot", priorityVal: null },
+		  { name: "player2", location: [14,8], bearing: [-1, 0], robot: "Spin Bot", priorityVal: null },
 		]
 
 		//seed for second location
 		var playerCardMove = [
-		  { name: "player1", location: [15,5], bearing: [0, 1], robot: "Hammer Bot", priorityVal: 500 },
-		  { name: "player3", location: [13,3], bearing: [-1, 0], robot: "Twonky", priorityVal: 800 },
-		  { name: "player2", location: [12,6], bearing: [-1, 0], robot: "Spin Bot", priorityVal: 200 },
+		  { name: "player3", location: [15,3], bearing: [-1, 0], robot: "Twonky", priorityVal: 800 },
+		  { name: "player1", location: [12,5], bearing: [-1, 0], robot: "Hammer Bot", priorityVal: 500 },
+		  { name: "player2", location: [11,8], bearing: [-1, 0], robot: "Spin Bot", priorityVal: 200 },
 		]
 
-		// var playerBoardMove = [
-		// 	{ name: "player3", location: [12,3], bearing: [1, 0], robot: "Twonky", priorityVal: 800 },
-		// 	{ name: "player1", location: [14,5], bearing: [-1, 0], robot: "Hammer Bot", priorityVal: 500 },
-		// 	{ name: "player2", location: [12, 6], bearing: [-1, 0], robot: "Spin Bot", priorityVal: 200 },
-		// ]
+		var playerBoardMove = [
+			// { name: "player3", location: [15,4], bearing: [-1, 0], robot: "Twonky", priorityVal: 800 }
+			// { name: "player1", location: [12,5], bearing: [-1, 0], robot: "Hammer Bot", priorityVal: 500 },
+			{ name: "player2", location: [10,8], bearing: [-1, 0], robot: "Spin Bot", priorityVal: 200 }
+		]
 
+				/* bearings
+
+				[-1,0] N
+				[0, 1] E
+				[0, -1] W
+				[1, 0] S
+
+				*/
 
 		var robotHash = {};
 
@@ -158,57 +166,94 @@ app.controller('GameCtrl', function($scope, $state, theGame){
 			      	robotHash[player.name] = robot;
 			      	robotHash[player.name].bearing = player.bearing;
 			      	renderer.render(stage)
-			      	// movePlayer();
 				}	
 			})
 				console.log('robohash', robotHash)
 		}
 
-		function cardMove(playerObjs) {
-			playerObjs.forEach(function(player,idx){
+		function move(playerObjs) {
+			playerObjs.reduce(function(acc, player, idx){
 				var robot = robotHash[player.name];
 				var turn = false;
+				var direction;
 				// setTimeout(turnAndMove.bind(null, player), idx*.5 + 6000)
 
-				turnAndMove()
+				return acc.then(function() {
+					return turnAndMove()	
+				});
 
 				function turnAndMove() {
 					turnRobot();
-					moveRobot();
+					return promiseForMoveRobot();
 				}
 
 				function turnRobot() {
-					var radians = Math.Pi/180, angle;
-					// robot.position.x += imgSize/2;
-					// robot.position.y += imgSize/2;
-					if(player.bearing[0] + robot.bearing[0] === 0 || player.bearing[1] + robot.bearing[1]) {
-						var container = new PIXI.Container();
-						// console.log('container', container)
+					if(player.bearing[0] !== robot.bearing[0] || player.bearing[1] !== robot.bearing[1]) {
+					var degreesToRotate = getRotation(robot.bearing, player.bearing);
+					// console.log('player name and rad to rotate', player.name, degreesToRotate)
+						var container = new Container();
+						container.position.x = robot.position.x + imgSize/2
+						container.position.y = robot.position.y + imgSize/2
 						container.pivot.set(robot.position.x + imgSize/2, robot.position.y + imgSize/2)
-						// container.anchor.set(robot.position.x + imgSize/2, robot.position.y + imgSize/2)
 						container.addChild(robot);
-						console.log('here', player.name)
-						// robot.anchor.x = imgSize/2;
-						// robot.anchor.y = imgSize/2;
-						container.rotation = Math.PI/2
-
+						container.rotation = degreesToRotate
 						stage.addChild(container)
-
+						turn = true;
 					}
-					// robot.position.x -=imgSize/2
-					// robot.position.y -= imgSize/2;
+
 					renderer.render(stage);
 				}
 
-				function moveRobot() {
-					if(!turn && robot.position.x >= imgSize * player.location[0]) {
-				        requestAnimationFrame(moveRobot);
+				function moveRobot(resolve) {
+					if(!turn && robot.position.x >= imgSize * player.location[0] && direction == 'north' || direction == undefined) {
+						direction = 'north';
+				        requestAnimationFrame(moveRobot.bind(null, resolve));
 				        robot.position.x -= 1;
 				        renderer.render(stage);
+				  	} 
+				  	else if(!turn && robot.position.x <= imgSize * player.location[0]) {
+				  		direction = "south";
+				  		// console.log('twonky')
+				  		// console.log(robot.position.x, imgSize*player.location[0])
+				  		requestAnimationFrame(function() {
+				  			moveRobot(resolve);
+				  		});
+				  		robot.position.x += 1;
+				  		renderer.render(stage);
+				  	}
+				  	else if(!turn && robot.position.y <= imgSize * player.location[1] && direction == 'west' || direction == undefined) {
+				  		direction = "west";
+				  		requestAnimationFrame(function() {
+				  			moveRobot(resolve);
+				  		});
+				  		robot.position.y += 1;
 				  	} 		
+				  	else if(!turn && robot.position.y <= imgSize & player.location[1] && direction == 'east' || direction == undefined) {
+				  		direction = 'east';
+				  		requestAnimationFrame(function() {
+				  			moveRobot(resolve);
+				  		});
+				  		robot.position.y -= 1;
+				  		renderer.render(stage);
+				  	} else {
+				  		resolve();
+				  	} 	
 				}	
-			})
+				function promiseForMoveRobot(){
+					return $q(function(resolve, reject){
+						moveRobot(resolve);	
+					});
+					
+				}
+
+			}, $q.resolve())
 		}
+
+		// function boardMove(playerObjs){
+		// 	playerObjs.forEach(function(player,idx){
+
+		// 	}
+		// }
 
 
 		buildTiles();
@@ -216,8 +261,11 @@ app.controller('GameCtrl', function($scope, $state, theGame){
 		drawDockLine();
 		drawLasers();
 		drawRobots(players);
-		cardMove(playerCardMove);
-		// newLocation(playerBoardMove);
+		move(playerCardMove);
+		console.log('robots now', robotHash)
+		setTimeout(function() {
+			move(playerBoardMove)
+		},5000);
 
 		function buildMap(){
 		  renderer.render(stage);
@@ -236,13 +284,14 @@ function robotImage (robotName) {
 	return robotName.toLowerCase().replace(/ /g,'') + '.png';
 }
 
+function getRotation (orig, next){
+	console.log('orig and next', orig, next)
+	if(orig[0] + next[0] ===  0 || orig[1] + next[1] === 0) return Math.PI;
+	else {
+	  var dot = -orig[0]*next[1] + orig[1]*next[0];
+	  console.log('dot', dot)
+	  var rad = Math.asin(dot);
+  	return rad;
+	}
+}
 
-
-/* bearings
-
-[-1,0] N
-[0, 1] E
-[0, -1] W
-[1, 0] S
-
-*/
